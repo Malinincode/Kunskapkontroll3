91fddb2e37d6972dbfd75e2ed87b5c0ad835fe2e
Malin Volle <malin.volle@gmail.com>
2022-01-27 12:04:39 +0100

    ändrar till e.target

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ let firstButton1 = document.getElementById("add-button");
 
 console.log(firstButton1)
 
-firstButton1.addEventListener('click', function() {
+firstButton1.addEventListener('click', function(e) {
 
     /* Skapar ny li */
     let newLi = document.createElement("li");
-    document.getElementById("todo-list").append(newLi);
+    e.target.nextSibling.nextSibling.childNodes[3].append(newLi);
+    console.log(e.target.previousSibling.previousSibling)
 
     /* Skapar nya textrutan */
     let input = document.createElement("input");
-    input.value = document.getElementById("input-syssla").value;
+    input.value = e.target.previousSibling.previousSibling.value;
     input.setAttribute("disabled", true);
     input.setAttribute("placeholder", "tom syssla");
     newLi.append(input);
@@ -31,18 +32,28 @@ firstButton1.addEventListener('click', function() {
     newLi.append(eraseButton);
 
     /* Skapar ny addEventListener */
-    changeButton.addEventListener('click', function(){
+    changeButton.addEventListener('click', function(e){
     
         /* Om låst, öppnar upp samt ändrar text */  
-        if(input.disabled){
+        if(e.target.previousSibling.disabled){
             input.disabled = false;
-            changeButton.innerHTML = "Spara";
+            e.target.innerHTML = "Spara";
             
          /* Om öppen, stänger samt ändrar text */  
         } else {
             input.disabled = true;
-            changeButton.innerHTML = "Ändra";
+            e.target.innerHTML = "Ändra";
         }
+
+    })
+
+
+    readyButton.addEventListener('click', function(e){
+       /*  console.log(readyButton) */
+        console.log(e.target.parentNode)
+      /*   console.log(document.getElementById("container")) */
+
+
 
     })
 })
