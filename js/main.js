5a75ac15622ebb9eb699df9c8d68aadb3fa4af2e
Malin Volle <malin.volle@gmail.com>
2022-01-27 14:28:14 +0100

    klar innan css

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ console.log(firstButton1)
 firstButton1.addEventListener('click', function(e) {
 
     /* Skapar ny li */
-    let newLi = document.createElement("li");
-    e.target.nextSibling.nextSibling.childNodes[3].append(newLi);
+    let li = document.createElement("li");
+    e.target.nextSibling.nextSibling.childNodes[3].append(li);
     console.log(e.target.previousSibling.previousSibling)
 
     /* Skapar nya textrutan */
@@ -14,22 +14,22 @@ firstButton1.addEventListener('click', function(e) {
     input.value = e.target.previousSibling.previousSibling.value;
     input.setAttribute("disabled", true);
     input.setAttribute("placeholder", "tom syssla");
-    newLi.append(input);
+    li.append(input);
 
     /* Skapar ny ändra knapp */
     let changeButton = document.createElement("button");
     changeButton.innerHTML = "Ändra";
-    newLi.append(changeButton);
+    li.append(changeButton);
 
     /* Skapar ny färdig knapp */
     let readyButton = document.createElement("button");
     readyButton.innerHTML = "Färdig";
-    newLi.append(readyButton);
+    li.append(readyButton);
 
     /* Skapar ny radera knapp */
     let eraseButton = document.createElement("button");
     eraseButton.innerHTML = "Radera";
-    newLi.append(eraseButton);
+    li.append(eraseButton);
 
     /* Skapar ny addEventListener */
     changeButton.addEventListener('click', function(e){
@@ -39,32 +39,28 @@ firstButton1.addEventListener('click', function(e) {
             e.target.previousSibling.disabled = false;
             e.target.innerHTML = "Spara";
             
-         /* Om öppen, stänger samt ändrar text */  
+        /* Om öppen, stänger samt ändrar text */  
         } else {
             e.target.previousSibling.disabled = true;
             e.target.innerHTML = "Ändra";
         }
-
     })
 
     /* Färdigknapp klar och flyttar över till sista listan, tar då bort färdigknappen*/  
     readyButton.addEventListener('click', function(e){
-       /*  console.log(readyButton) */
+        /*  console.log(readyButton) */
         console.log(e.target.parentNode)
         console.log(e.target.parentNode.parentNode.parentNode.nextSibling.nextSibling.childNodes[3])
         e.target.parentNode.parentNode.parentNode.nextSibling.nextSibling.childNodes[3].append(e.target.parentNode);
-      /*   console.log(document.getElementById("container")) */
-      console.log(readyButton)
-      e.target.remove();
-
-
+        /*   console.log(document.getElementById("container")) */
+        console.log(readyButton)
+        e.target.remove();
     })
 
-    /* raderar via knappen Radera */  
+    /* Raderar via knappen Radera */  
     eraseButton.addEventListener('click', function(e){
-         console.log(e.target.parentNode)
-         e.target.parentNode.remove();
+        console.log(e.target.parentNode)
+        e.target.parentNode.remove();
  
      })
-
 })
